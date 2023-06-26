3454d99f17754d5451face9885d28730649646b7
Aram Akhavan <github@aram.nubmail.ca>
2023-06-26 11:50:21 -0700

    Update humantime.fish

--- tests/humantime.fish
+++ tests/humantime.fish
@@ -1,8 +1,8 @@
 functions --query humantime || exit
 
 @test "milliseconds" (humantime 1) = 1ms
-@test "scale=1 seconds" (humantime 60) = 0.1s
 @test "seconds" (humantime 1000) = 1s
+@test "fractional seconds" (humantime 1100) = 1.1s
 @test "minutes" (humantime 60000) = 1m
 @test "hours" (humantime 3600000) = 1h
 @test "hours seconds" (humantime 3601000) = "1h 1s"
