9c88c0ec80b18277138c31200e2596689476fd59
Aram Akhavan <github@aram.nubmail.ca>
2023-06-26 12:11:40 -0700

    Update humantime.fish

--- functions/humantime.fish
+++ functions/humantime.fish
@@ -4,7 +4,7 @@ function humantime --argument-names ms --description "Turn milliseconds into a h
     test (math --scale=0 $ms/1000) -lt 1 && echo $ms"ms" && return
 
     set --local secs (math --scale=1 $ms/1000)
-    set --local mins (math --scale=0 $ms/60000 % 60)
+    set --local mins (math --scale=0 $ms/60000)
     set --local hours (math --scale=0 $ms/3600000)
 
     test $hours -gt 0 && set --local --append out $hours"h"
