eaf6bd294ec5de5b1e2e83540e76b1a9e40883dd
Aram Akhavan <github@aram.nubmail.ca>
2023-06-27 09:29:07 -0700

    Update humantime.fish

--- functions/humantime.fish
+++ functions/humantime.fish
@@ -1,10 +1,10 @@
 function humantime --argument-names ms --description "Turn milliseconds into a human-readable string"
     set --query ms[1] || return
 
-    test (math --scale=0 $ms/1000) -lt 1 && echo $ms"ms" && return
+    test $ms -lt 1000 && echo $ms"ms" && return
 
-    set --local secs (math --scale=1 $ms/1000)
-    set --local mins (math --scale=0 $ms/60000)
+    set --local secs (math --scale=1 $ms/1000 % 60)
+    set --local mins (math --scale=0 $ms/60000 % 60)
     set --local hours (math --scale=0 $ms/3600000)
 
     test $hours -gt 0 && set --local --append out $hours"h"
