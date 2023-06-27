4a2f112fd144177a6172a123fbef7010f86e6a80
Aram Akhavan <github@aram.nubmail.ca>
2023-06-27 09:31:00 -0700

    Show ms up to 1s

--- functions/humantime.fish
+++ functions/humantime.fish
@@ -1,6 +1,8 @@
 function humantime --argument-names ms --description "Turn milliseconds into a human-readable string"
     set --query ms[1] || return
 
+    test $ms -lt 1000 && echo $ms"ms" && return
+
     set --local secs (math --scale=1 $ms/1000 % 60)
     set --local mins (math --scale=0 $ms/60000 % 60)
     set --local hours (math --scale=0 $ms/3600000)
@@ -9,5 +11,5 @@ function humantime --argument-names ms --description "Turn milliseconds into a h
     test $mins -gt 0 && set --local --append out $mins"m"
     test $secs -gt 0 && set --local --append out $secs"s"
 
-    set --query out && echo $out || echo $ms"ms"
+    echo $out
 end
--- tests/humantime.fish
+++ tests/humantime.fish
@@ -1,11 +1,14 @@
 functions --query humantime || exit
 
 @test "milliseconds" (humantime 1) = 1ms
-@test "scale=1 seconds" (humantime 60) = 0.1s
+@test "milliseconds 999" (humantime 999) = 999ms
 @test "seconds" (humantime 1000) = 1s
+@test "fractional seconds" (humantime 1123) = 1.1s
 @test "minutes" (humantime 60000) = 1m
 @test "hours" (humantime 3600000) = 1h
 @test "hours seconds" (humantime 3601000) = "1h 1s"
+@test "hours fractional seconds" (humantime 3601200) = "1h 1.2s"
 @test "hours minutes" (humantime 3660000) = "1h 1m"
 @test "hours minutes seconds" (humantime 3661000) = "1h 1m 1s"
+@test "hours minutes fractional seconds" (humantime 3661500) = "1h 1m 1.5s"
 @test "pie" (humantime 11655900) = "3h 14m 15.9s"
